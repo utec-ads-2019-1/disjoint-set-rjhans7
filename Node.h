feba97868eca1b76cba37a12e51e2afb152923ff
Rjhans7 <roosevelt.ubaldo@outlook.com>
2019-05-06 20:59:26 -0500

    Last Commit

--- Node.h
+++ Node.h
@@ -8,6 +8,7 @@ template <typename T>
 class Node{
     T data;
     int rank;
+    int size;
     Node <T>* parent;
 
 public:
@@ -15,15 +16,57 @@ public:
         data = value;
         parent = this;
         rank = 0;
+        size = 0;
     }
     void addParent(Node <T>* padre){
         parent = padre;
         padre->rank=1;
+        padre->size++;
     };
-    void arbUnion(Node<T> nuevo){
-        nuevo.parent = this;
-        rank+=nuevo.rank;
+    void arbUnion(Node<T>* nuevo){
+        nuevo->parent = this;
+        rank+=nuevo->rank;
+        size+=nuevo->size+1;
     }
+    void killDuplicated(){
+        auto temp = parent;
+
+
+    }
+    void rankUnion(Node<T>* nuevo){
+        if(nuevo->rank > this->rank){
+            parent=nuevo->parent;
+            nuevo->rank+=rank;
+            nuevo->size=size+1;
+        }else{
+            nuevo->parent=this;
+            rank+=nuevo->rank;
+            size+=nuevo->size+1;
+        }
+
+    }
+    bool find(T valor){
+        auto temp = parent;
+        while(temp->parent!=temp){
+            if(temp->data==valor)
+                return true;
+        temp = temp->parent;
+        }
+        return false;
+    };
+
+    void sizeUnion(Node<T>* nuevo){
+        if(nuevo->size > this->size){
+            parent=nuevo->parent;
+            nuevo->rank+=rank;
+            nuevo->size=size+1;
+        }else{
+            nuevo->parent=this;
+            rank+=nuevo->rank;
+            size+=nuevo->size+1;
+        }
+    }
+
     void print(){
         cout<<"Node: "<<data<< " Parent: "<<parent->data<<" Rank: "<<rank <<endl;
     }
